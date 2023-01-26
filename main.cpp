102ecbcf4ef9d44ab6a0cfd869fc85e8cf730305
Daniel Hong <rhong@zagmail.gonzaga.edu>
2023-01-26 22:56:37 +0000

    initial

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "vecfuncs.h"
+
+int main() {
+  vector<int> userNums;
+  fillVector(userNums);
+  cout << "Size: " << userNums.size() << endl;
+
+  int min, max;
+
+  findExtremes(userNums, min, max);
+  cout << "min: " << min << " max: " << max << endl;
+  
+}
